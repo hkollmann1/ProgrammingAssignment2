b536aa8c6c62591d8eba28a998e4fd4c06df5538
hkollmann1 <heinz.kollmann@mail.udp.cl>
2020-09-09 18:36:05 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+ ##The MakeCacheMatrix function is used to create the special matrix 
+##to obtain the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 m <- NULL
@@ -18,7 +16,8 @@ m <- NULL
 }
 
 
-## Write a short comment describing this function
+## The cachesolve function calculates the inverse 
+##of the matrix obtained by the MakeCacheMatrix function
 
 cacheSolve <- function(x, ...) {
 	m <- x$getsolve()
@@ -31,3 +30,22 @@ cacheSolve <- function(x, ...) {
   x$setsolve(m)
         ## Return a matrix that is the inverse of 'x'
 }
+
+## my test code
+##m1 <- matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2)
+##> mymatrix <- makeCacheMatrix(m1)
+##  > mymatrix$get()
+##[,1]  [,2]
+##[1,]  0.50 -1.00
+##[2,] -0.25  0.75
+##> cacheSolve(mymatrix)
+##obteniendo datos del cache
+##[,1] [,2]
+##[1,]    6    8
+##[2,]    2    4
+##> n2 <- matrix(c(5/8, -1/8, -7/8, 3/8), nrow = 2, ncol = 2)
+##> mymatrix$set(n2)
+##obteniendo datos del cache
+##[,1] [,2]
+##[1,]    3    7
+##[2,]    1    5
